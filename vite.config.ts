ca8da4367bd7a5f3c9b90eb2d82a6fc289c29c27
Akmar-Azmi <nurakmarazmi15@gmail.com>
2026-06-27 02:52:41 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
    vite: {
-    base: "most-wanted-ai",
+    base: "Portfolio_NurAkmar",
   },
 
   tanstackStart: {
