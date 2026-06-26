dbae9080b2f09b28d041f7a767a99cc4c0643767
Akmar-Azmi <nurakmarazmi15@gmail.com>
2026-06-27 02:54:51 +0800

    update package.json

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
    vite: {
-    base: "Portfolio_NurAkmar",
+    base:"/Portfolio_NurAkmar/",
   },
 
   tanstackStart: {
